45864c7e7d56703f6b3649da7a05de1ff304cfd9
JoanValiente <joan.valiente.lorite@gmail.com>
2020-01-21 14:53:42 +0100

    sewgrh

---
+++ CitmThirdGameJam/Assets/BulletMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BulletMovement : MonoBehaviour
+{
+    enum PlayerDirection
+    {
+        UP,
+        DOWN,
+        RIGHT,
+        LEFT
+    }
+
+    public float bulletSpeed = 0.0f;
+    Rigidbody2D rb;
+    PlayerDirection pd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pd = PlayerDirection.UP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (pd == PlayerDirection.UP)
+        {
+            pos.y += bulletSpeed * Time.deltaTime;
+        }
+        else if (pd == PlayerDirection.DOWN)
+        {
+            pos.y -= bulletSpeed * Time.deltaTime;
+        }
+        else if (pd == PlayerDirection.RIGHT)
+        {
+            pos.x += bulletSpeed * Time.deltaTime;
+        }
+        else if (pd == PlayerDirection.LEFT)
+        {
+            pos.x -= bulletSpeed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
---
+++ CitmThirdGameJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 0.0f;
+
+    public GameObject shadow1 = null;
+    private GameObject shadow_child = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InstanciateShadow();
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+            shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.UP);
+        }
+        else if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+            shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.DOWN);
+        }
+        else if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+            shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.RIGHT);
+        }
+        else if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+            shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.LEFT);
+        }
+        else
+        {
+            shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.WAIT);
+        }
+
+
+        transform.position = pos;
+    }
+
+    private void InstanciateShadow()
+    {
+        if (shadow1 != null)
+            shadow_child = Instantiate(shadow1);
+
+        shadow_child.GetComponent<ShadowBehaviour>().setVel(speed);
+    }
+}
