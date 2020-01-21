8d8a34fb2c72546bb1b0d3c9d7984496a9e0ed5a
JoanValiente <joan.valiente.lorite@gmail.com>
2020-01-21 13:58:43 +0100

    Blue square moving

---
+++ CitmThirdGameJam/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        else if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        else if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        else if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
