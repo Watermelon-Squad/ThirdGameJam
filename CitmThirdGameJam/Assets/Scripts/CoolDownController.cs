21895098984672d4a9fca405cc58d3850c0582d1
JoanValiente <joan.valiente.lorite@gmail.com>
2020-01-22 14:30:49 +0100

    cooldown?

---
+++ CitmThirdGameJam/Assets/Scripts/CoolDownController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoolDownController : MonoBehaviour
+{
+    public Image imageCooldown;
+    public float cooldown = 2.0f;
+    bool isCooldown;
+
+    public GameObject player = null;
+    private Animator anim = null;
+
+    private void Start()
+    {
+        anim = player.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (anim.GetBool("Shoot"))
+        {
+            isCooldown = true;
+        }
+
+        if (isCooldown)
+        {
+            imageCooldown.fillAmount += 1 / cooldown * Time.deltaTime;
+
+            if (imageCooldown.fillAmount >= 1)
+            {
+                imageCooldown.fillAmount = 0;
+                isCooldown = false;
+            }
+        }
+        
+    }
+}
