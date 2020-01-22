3da03d65e50f5a5750928ffe92c1bead30059293
Siitoo <alfonsosanchez1896@gmail.com>
2020-01-22 08:14:22 +0100

    Solve P2 animations

--- CitmThirdGameJam/Assets/Scripts/SecondPlayerController.cs
+++ CitmThirdGameJam/Assets/Scripts/SecondPlayerController.cs
@@ -5,6 +5,21 @@ using XInputDotNetPure;
 
 public class SecondPlayerController : MonoBehaviour
 {
+    public enum AnimStates
+    {
+        UP,
+        DOWN,
+        RIGHT,
+        LEFT,
+        IDLE_NONE
+    }
+
+    public KeyCode up_input;
+    public KeyCode down_input;
+    public KeyCode left_input;
+    public KeyCode right_input;
+    public KeyCode shoot_input;
+
     public float speed = 0.0f;
 
     public GameObject shadow1 = null;
@@ -27,7 +42,7 @@ public class SecondPlayerController : MonoBehaviour
     GamePadState state;
 
     private GameSceneManage sceneManaging = null;
-
+    private AnimStates lastState = AnimStates.IDLE_NONE;
     // Start is called before the first frame update
     void Start()
     {
@@ -52,45 +67,86 @@ public class SecondPlayerController : MonoBehaviour
 
             if (actual_shoot_time >= shoot_time)
             {
-                if (state.ThumbSticks.Right.Y > 0.1f || Input.GetKey(KeyCode.UpArrow))
+                if (state.ThumbSticks.Right.Y > 0.1f || Input.GetKey(up_input))
                 {
                     rigidbody2D.AddForce(Vector2.up * speed * Time.deltaTime);
+                    // pos.y += speed * Time.deltaTime;
                     shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.UP);
                     animator.SetInteger("State", 0);
+                    lastState = AnimStates.UP;
+
+                    if (animator.GetBool("Idle"))
+                    {
+                        animator.SetBool("Idle", false);
+                    }
                 }
-                else if (state.ThumbSticks.Right.Y < -0.1f || Input.GetKey(KeyCode.DownArrow))
+                else if (state.ThumbSticks.Right.Y < -0.1f || Input.GetKey(down_input))
                 {
                     rigidbody2D.AddForce(Vector2.down * speed * Time.deltaTime);
+                    //pos.y -= speed * Time.deltaTime;
                     shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.DOWN);
                     animator.SetInteger("State", 1);
+                    lastState = AnimStates.DOWN;
+
+                    if (animator.GetBool("Idle"))
+                    {
+                        animator.SetBool("Idle", false);
+                    }
                 }
-                else if (state.ThumbSticks.Right.X > 0.1f || Input.GetKey(KeyCode.RightArrow))
+                else if (state.ThumbSticks.Right.X > 0.1f || Input.GetKey(right_input))
                 {
                     rigidbody2D.AddForce(Vector2.right * speed * Time.deltaTime);
+                    //pos.x += speed * Time.deltaTime;
                     shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.RIGHT);
                     animator.SetInteger("State", 2);
+                    lastState = AnimStates.RIGHT;
+
+                    if (animator.GetBool("Idle"))
+                    {
+                        animator.SetBool("Idle", false);
+                    }
+                    //sprite.flipX = false;
                 }
-                else if (state.ThumbSticks.Right.X < -0.1f || Input.GetKey(KeyCode.LeftArrow))
+                else if (state.ThumbSticks.Right.X < -0.1f || Input.GetKey(left_input))
                 {
                     rigidbody2D.AddForce(Vector2.left * speed * Time.deltaTime);
+                    //pos.x -= speed * Time.deltaTime;
                     shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.LEFT);
                     animator.SetInteger("State", 3);
+                    lastState = AnimStates.LEFT;
+
+                    if (animator.GetBool("Idle"))
+                    {
+                        animator.SetBool("Idle", false);
+                    }
+                    //sprite.flipX = true;
                 }
                 else
                 {
-                    animator.SetBool("Idle", true);
+                    if (!animator.GetBool("Idle"))
+                    {
+                        animator.SetBool("Idle", true);
+                    }
+                    animator.SetInteger("State", -1);
                     shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.WAIT);
                 }
+
             }
             else
             {
-                actual_shoot_time += Time.deltaTime;
+                if (!animator.GetBool("Idle"))
+                {
+                    animator.SetBool("Idle", true);
+                }
+                animator.SetInteger("State", -1);
                 shadow_child.GetComponent<ShadowBehaviour>().SetPlayerInput(ShadowBehaviour.PlayerInput.WAIT);
+                actual_shoot_time += Time.deltaTime;
             }
+
 
             if (actual_cooldown >= cooldown_shoot)
             {
-                if (state.Buttons.RightShoulder == ButtonState.Pressed || Input.GetKeyDown("m"))
+                if (state.Buttons.RightShoulder == ButtonState.Pressed || Input.GetKey(shoot_input))
                 {
                     actual_cooldown = 0.0f;
                     actual_shoot_time = 0.0f;
@@ -106,7 +162,7 @@ public class SecondPlayerController : MonoBehaviour
 
     private void Fire()
     {
-        bulletPos = transform.position;
+        animator.SetBool("Shoot", true);
 
         float shootOffset = 0.3f;
         bulletPos = transform.position;
@@ -115,22 +171,22 @@ public class SecondPlayerController : MonoBehaviour
 
         newBullet.GetComponent<BulletMovement>().parentTag = transform.tag;
 
-        if (animator.GetInteger("State") == 0)
+        if (animator.GetInteger("State") == 0 || lastState == AnimStates.UP)
         {
             newBullet.GetComponent<BulletMovement>().pd = BulletMovement.PlayerDirection.UP;
             newBullet.transform.position = new Vector2(bulletPos.x, bulletPos.y + shootOffset);
         }
-        else if (animator.GetInteger("State") == 1)
+        else if (animator.GetInteger("State") == 1 || lastState == AnimStates.DOWN)
         {
             newBullet.GetComponent<BulletMovement>().pd = BulletMovement.PlayerDirection.DOWN;
             newBullet.transform.position = new Vector2(bulletPos.x, bulletPos.y - shootOffset);
         }
-        else if (animator.GetInteger("State") == 2)
+        else if (animator.GetInteger("State") == 2 || lastState == AnimStates.RIGHT)
         {
             newBullet.GetComponent<BulletMovement>().pd = BulletMovement.PlayerDirection.RIGHT;
             newBullet.transform.position = new Vector2(bulletPos.x + shootOffset, bulletPos.y);
         }
-        else if (animator.GetInteger("State") == 3)
+        else if (animator.GetInteger("State") == 3 || lastState == AnimStates.LEFT)
         {
             newBullet.GetComponent<BulletMovement>().pd = BulletMovement.PlayerDirection.LEFT;
             newBullet.transform.position = new Vector2(bulletPos.x - shootOffset, bulletPos.y);
@@ -158,6 +214,11 @@ public class SecondPlayerController : MonoBehaviour
         Destroy(shadow_child);
         //Create new shadow in last player position
         InstanciateShadow(transform.position);
+    }
+
+    public void stopShoot()
+    {
+        animator.SetBool("Shoot", false);
     }
 
 }
